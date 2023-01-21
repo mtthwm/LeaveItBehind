5bc4e0321299aaa08baa68513b0ea40157c57d8a
AJNaito <89541191+AJNaito@users.noreply.github.com>
2023-01-21 13:51:20 -0700

    Switched movement

--- Assets/Scripts/General Classes/MovementController.cs
+++ Assets/Scripts/General Classes/MovementController.cs
@@ -6,26 +6,28 @@ using UnityEngine;
 /// Assumes all speeds are adjusted for fixedDeltaTime -- USE IN FIXED UPDATE
 /// **IMPORTANT** MovePosition = KINEMATIC RIGIDBODY!!!
 /// If Rigidbody not kinemetic change to rb.velocity = Vector2.___ * speed;
+/// 
+/// 1/21 - Switched to velocity based to move with collisions
 /// </summary>
 public class MovementController : MonoBehaviour
 {
     public static void Move_Left(Rigidbody2D rb, float speed)
     {
-        rb.MovePosition(Vector2.left * speed);
+        rb.velocity = (Vector2.left * speed);
     }
 
     public static void Move_Right(Rigidbody2D rb, float speed)
     {
-        rb.MovePosition(Vector2.right * speed);
+        rb.velocity= (Vector2.right * speed);
     }
 
     public static void Move_Up(Rigidbody2D rb, float speed)
     {
-        rb.MovePosition(Vector2.up * speed);
+        rb.velocity = (Vector2.up * speed);
     }
 
     public static void Move_Down(Rigidbody2D rb, float speed)
     {
-        rb.MovePosition(Vector2.down * speed);
+        rb.velocity = (Vector2.down * speed);
     }
 }
